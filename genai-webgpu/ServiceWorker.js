6375f4a16f547a6381669e1346a95e774b041f11
voidwave <majed@voidwave.com>
2025-05-05 08:54:26 +0300

    game genai

---
+++ genai-webgpu/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "voidwave-Project Depth Test-0.1.1";
+const contentToCache = [
+    "Build/genai-webgpu.loader.js",
+    "Build/genai-webgpu.framework.js.br",
+    "Build/genai-webgpu.data.br",
+    "Build/genai-webgpu.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});

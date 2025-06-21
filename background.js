eb56a72115696f3bc56ddb8623faac1d6383ef4e
voidwave <majed@voidwave.com>
2025-06-21 13:03:11 +0300

    Update background.js

--- background.js
+++ background.js
@@ -12,10 +12,10 @@ font.load().then(function (loadedFont) {
 	canvas.height = window.innerHeight;
 
 	const arabic = 'سذضصثقفغعهخحجدطكمنتالبيسشئءؤرلاىةوزظا';
-	const latin = 'QWERTYUIOPASDFGHJKLZXCVBNM';
-	const nums = '0123456789';
+	// const english = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+	// const nums = '0123456789';
 
-	const alphabet = arabic;// + latin + nums;
+	const alphabet = arabic;// + english + nums;
 
 	const fontSize = 13;
 	const columns = canvas.width / fontSize;
@@ -25,7 +25,7 @@ font.load().then(function (loadedFont) {
 	var mColor = '#0F0';
 
 	for (let x = 0; x < columns; x++) {
-		rainDrops[x] = 1;
+		rainDrops[x] = canvas.height;
 	}
 
 
@@ -36,7 +36,7 @@ font.load().then(function (loadedFont) {
 		context.font = fontSize + 'px SaudiWeb';
 
 		for (let i = 0; i < rainDrops.length; i++) {
-			// If the current character is one of the letters "VOIDWAE", set the fill style to white.
+			// If the current character is one of the letters "VOIDWAVE", set the fill style to white.
 			// Otherwise, set it to green.
 			const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
 			context.fillStyle = mColor;
